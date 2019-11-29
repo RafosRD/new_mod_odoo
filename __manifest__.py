c1bba97735186920aee6dcc932c133183b7e9b41
Raul Ovalle <raulovallet@gmail.com>
2019-11-29 12:39:02 -0500

    new features

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,21 @@
     'version': '',
     'summary': '',
     'description': '',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
     'category': '',
-    'author': 'Raul Ovalle, rovalle@guavana.com',
-    'website': 'www.guavana.com',
+    'author': '',
+    'website': '',
     'license': '',
-    'depends': [''],
-    'data': [''],
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    'demo': [],
     'installable': True,
     'auto_install': False,
 }
